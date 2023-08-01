eedca5e93ac5b2522834a8dac84e199c4591c9e8
Divya097 <129171404+Divya097@users.noreply.github.com>
2023-08-01 20:07:54 +0530

    Update insert any.cpp

--- insert any.cpp
+++ insert any.cpp
@@ -3,41 +3,33 @@
 
 int main()
 {
-    //2
     int size;
     int newNumber, position;
     int i;
     int numArray[MAX_ARRAY_SIZE];
 
-    //3
     printf("Enter size of the array : ");
     scanf("%d", &size);
 
-    //4
     for (i = 0; i < size; i++)
     {
         printf("Enter value for position %d : ", i);
         scanf("%d", &numArray[i]);
     }
 
-    //5
     printf("Enter the number you want to add to this array : ");
     scanf("%d", &newNumber);
 
-    //6
     printf("Enter the position in the array to add this number : ");
     scanf("%d", &position);
 
-    //7
     for (i = size; i >= position - 1; i--)
     {
         numArray[i + 1] = numArray[i];
     }
 
-    //8
     numArray[position - 1] = newNumber;
 
-    //9
     for (i = 0; i < size + 1; i++)
     {
         printf("%d ", numArray[i]);
